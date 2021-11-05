8d02a478e2e6aebee972b5dfe5ca3787fb23887e
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-11-05 05:03:02 -0300

    Aula 2 Finalizada!

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,13 +1,66 @@
 package br.com.alura.spring.data;
 
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringDataApplication {
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.service.CrudCargoService;
+import br.com.alura.spring.data.service.CrudFuncionarioService;
+import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
 
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner {
+	
+	private final CrudCargoService cargoService;
+	
+	private final CrudFuncionarioService funcionarioService;
+
+	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+	private Boolean system = true;
+	
+	public SpringDataApplication(CrudCargoService cargoService, CrudFuncionarioService funcionarioService, 
+			CrudUnidadeTrabalhoService unidadeTrabalhoService) {
+		this.cargoService = cargoService;
+		this.funcionarioService = funcionarioService;
+		this.unidadeTrabalhoService = unidadeTrabalhoService;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		while(system) {
+			System.out.println("Qual acao voce quer executar");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+			
+			Integer function = scan.nextInt();
+			
+			switch (function) {
+			case 1:
+				cargoService.inicial(scan);
+				break;
+			case 2:
+				funcionarioService.inicial(scan);
+				break;
+			case 3:
+				unidadeTrabalhoService.inicial(scan);
+				break;
+			default:
+				System.out.println("Finalizando");
+				system = false;
+				break;
+		}
+		}
+
 	}
 
 }
